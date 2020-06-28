ea0c96336bbb7237609b425b0f6da089fb03c467
isaac_lee <yy0221ss@gmail.com>
2020-06-28 10:02:25 +0900

    commit via mac

--- 25Timer/ViewController.swift
+++ 25Timer/ViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var WebViewMain: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // 1. url string
+        // 2. url > request
+        // 3. req > load
+        
+        let urlString = "https://www.google.com"
+        if let url = URL(string: urlString) {
+            let urlReq = URLRequest(url: url)
+            WebViewMain.load(urlReq)
+        }
+        
     }
 
-
+    
+    
 }
 
